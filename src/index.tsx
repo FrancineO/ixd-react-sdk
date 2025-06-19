383bf26d2ba947ed8f57b27e050de37240c69d79
Francine Ong <francine.o@live.com>
2025-06-19 15:32:52 +0200

    remove hardcoded links

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,10 @@ import { render } from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import TopLevelApp from '../src/samples/TopLevelApp';
 import './common.css';
+import './assets/img/favicon.ico';
+import './assets/css/sdkStyles.css';
+import './assets/css/variables.css';
+import
 
 const outletElement = document.getElementById('outlet');
 
